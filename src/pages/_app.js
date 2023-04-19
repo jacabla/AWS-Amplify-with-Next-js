52c1cd16c82797b8d51e72fe0d05905da428303d
Jason <castrob.jason@gmail.com>
2023-04-19 10:43:43 -0600

    template is up

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import "../styles/tailwind.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
